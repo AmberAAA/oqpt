909a30c00e07a15057598d42b30a96d5da020678
Amber <anborong1314@gmail.com>
2024-05-29 15:57:52 +0800

    .

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, sqliteTable, text, g } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const todos = sqliteTable("todos", {
   id: integer("id").primaryKey(),
